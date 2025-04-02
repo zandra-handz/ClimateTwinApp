83e19f7ef8c49861153e36014de1616c3e431181
zandra_handz <taylorcolegrove@gmail.com>
2025-04-01 22:27:11 -0400

    removed sentry stuff for now because eas build failed and I don't have time tonight to fix

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -5,13 +5,13 @@ import HomeHeader from "../components/HeaderComponents/HomeHeader";
 
 import DrawerCustomizer from "../components/DrawerNavComponents/DrawerCustomizer";
 
-import * as Sentry from "@sentry/react-native";
+// import * as Sentry from "@sentry/react-native";
 
-Sentry.init({
-  dsn: "https://b003b07ef14d51d700aac9ce83006d95@o4509079411752960.ingest.us.sentry.io/4509079412801536",
-  enableInExpoDevelopment: true, // Set to true if using Expo Go
-  debug: true, // Enable debugging logs
-});
+// Sentry.init({
+//   dsn: "https://b003b07ef14d51d700aac9ce83006d95@o4509079411752960.ingest.us.sentry.io/4509079412801536",
+//   enableInExpoDevelopment: true, // Set to true if using Expo Go
+//   debug: true, // Enable debugging logs
+// });
 
 
 export default () => {
--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getDefaultConfig } = require('expo/metro-config');
 //const { getDefaultConfig } = require('@react-native/metro-config');
 
-const config = getSentryExpoConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 const { transformer, resolver } = config;
 
